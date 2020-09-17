3af102ccf1c3b2c22717a5cab57d71911d7ece55
=itschrisnguyen <itschrisnguyen@gmail.com>
2020-09-16 20:39:22 -0700

    update 2

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
-def nth_power(n,power):
+def nth_power(n,power=2):
     """
     calculate the power for numbers up to n
     """
 
     return [i**power for i in range(n)]
 
-print(nth_power(10,4))
+print(nth_power(10))
