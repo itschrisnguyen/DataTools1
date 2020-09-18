6724bf00e445679267878ac75ad2bc6645a1f046
=itschrisnguyen <itschrisnguyen@gmail.com>
2020-09-18 15:19:05 -0700

    added scaled sigmoid

--- util.py
+++ util.py
@@ -1,4 +1,11 @@
+import numpy as np
+
 def nth_power(n,power):
     return [i**power for i in range(n)]
 
 print(nth_power(10,4))
+
+def sigmoid(x, a=1):
+    return 1/(1+np.exp(-a*x))
+
+print(sigmoid(0.0))
